19cad081f95d776f316904204c9f214ff09cb57d
andmatsou <andmatsou@gmail.com>
2022-06-01 21:04:32 -0300

    Feat: adicionado os hooks useState e useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Menu from './components/Menu';
 import Article from './components/Article';
@@ -17,6 +17,28 @@ function App() {
     }
   }
 
+  const [nome, setNome] = useState("Anderson");
+  const [produtoId, setProdutoId] = useState(3);
+  const [produtoNome, setProdutoNome] = useState("");
+  const [data, setData] = useState({
+    nome: '',
+    preco: ''
+  })
+
+  function buscarProduto() {
+    console.log("Procurar produto");
+    setProdutoId(4); // ao setar um novo idproduto o useEffect é executado novamente
+    setProdutoNome("Curso de react");
+    setData({
+      nome: "Curso React.js",
+      preco: 245
+    })
+  }
+
+  useEffect(() => {
+    buscarProduto();
+  },[produtoId]);
+
   return(
     <div>
       <Header />
@@ -29,6 +51,17 @@ function App() {
       />
       <Aside />
       <Footer />
+      <hr />
+        <p>Hooks - Usestate</p><br /><br />
+        <p>{nome}</p>
+        <button onClick={() => setNome("Anderson Mathias")}>Alterar</button>
+      <hr />
+      <p>Listar Produto</p>
+      <p>{produtoNome}</p>
+
+      <p>Listar Cursos</p>
+      <p>{data.nome}</p>
+      <p>{data.preco}</p>
     </div>
   );
 }
